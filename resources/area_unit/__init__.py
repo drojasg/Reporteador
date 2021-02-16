3ce545bf5a0436ed57686cb39d1b724b3403cf62
IsisRamon <isis.judith.ramon@gmail.com>
2021-02-16 16:33:02 -0500

    Api get def property

--- resources/area_unit/__init__.py
+++ resources/area_unit/__init__.py
@@ -1,6 +1,7 @@
 from config import api
 from .area_unit import AreaUnitListSearch, AreaUnit
 from .book_address import BookAddressListSearch, BookAddress
+from .def_property import DefPropertyListSearch, DefProperty
 
 #urls apis basicas
 api.add_resource(AreaUnit, '/api/area-unit/create',endpoint="api-area-unit-post", methods=["POST"])
@@ -14,3 +15,8 @@ api.add_resource(AreaUnit, '/api/pms/get/<int:id>',endpoint="api-area-unit-get-b
 #Booking_address
 api.add_resource(BookAddress, '/api/address/search/<int:id>',endpoint="api-book-address-get-by-id", methods=["GET"])
 api.add_resource(BookAddressListSearch, '/api/address/get',endpoint="api-area-address-get-all", methods=["GET"])
+
+#def_property
+api.add_resource(DefProperty, '/api/property/search/<int:id>',endpoint="api-def-property-get-by-id", methods=["GET"])
+api.add_resource(DefPropertyListSearch, '/api/property/get',endpoint="api-def-property-get-all", methods=["GET"])
+
