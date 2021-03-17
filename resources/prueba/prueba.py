279f2e3c7c0f876d76f461e360db29ab0ded843f
danirojasglz <danirojasglz@gmail.com>
2021-03-17 17:49:19 -0500

    agrego archivo de api modificado para trabajar en casa

--- resources/prueba/prueba.py
+++ resources/prueba/prueba.py
@@ -9,39 +9,26 @@ from dateutil.parser import parse
 from common.public_auth import PublicAuth
 
 class PruebaSearch(Resource):
-   def getBookEstado1(self, estado):
+   def getBookEstado1(estado):
         response = {}
         try:
-            json_data = request.get_json(force=True)
-            schema = pModel.query.filter(pModel.estado == 1)
-            data = schema.load(json_data)
-            model = Model.query.get(estado)
-            
-            if model is None:
-                return {
+            schema = ModelSchema(exclude=Util.get_default_excludes())
+            data = Model.query.get(estado)
+            response = {
+                "Code": 200,
+                "Msg": "Success",
+                    "Error": False,
+                    "data": schema.dump(data)
+            }
+            else:
+            response ={
                     "Code": 404,
                     "Msg": "data not found",
                     "Error": True,
                     "data": {}
                     }
 
-            result = schema.dump(model)
-
-            response = {
-                "Code": 200,
-                "Msg": "Success",
-                "Error": False,
-                "data": result
-            }
-        except ValidationError as error:
-                response = {
-                    "Code": 500,
-                    "Msg": error.messages,
-                    "Error": True,
-                    "data": {}
-                }
         except Exception as e:
-                db.session.rollback()
                 response = {
                     "Code": 500,
                     "Msg": str(e),
