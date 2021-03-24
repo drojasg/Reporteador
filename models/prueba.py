be296688d8417cb8b08a1c78b4cdee1e64d0b0b6
danirojasglz <danirojasglz@gmail.com>
2021-03-24 17:48:46 -0500

    agrego todas las modificaciones realizadas

--- models/prueba.py
+++ models/prueba.py
@@ -5,46 +5,38 @@ from marshmallow import Schema, fields, validate
 from .policy import Policy, PolicySchema
 from common.util import Util
 
-book_hotel_properties = db.Table('book_hotel',
-    db.column('iddef_property', db.Integer, db.ForeignKey('def_property.iddef_property'),primary_key = True),
-    db.column('iddef_market_segment', db.Integer, db.ForeignKey('def_market_segment.iddef_market_segment'), primary_key = True),
-    db.column('iddef_country', db.Integer, db.ForeignKey('def_country.iddef_country'), primary_key = True),
-    db.column('iddef_channel', db.Integer, db.ForeignKey('def_channel.iddef_channel') primary_key = True),
-    db.column('iddef_currency', db.Integer, db.ForeignKey('def_currency.iddef_currency') primary_key = True),
-    db.column('iddef_language', db.Integer, db.ForeignKey('def_language.iddef_language') primary_key = True),
-    db.column('idbook_status', db.Integer, db.ForeignKey('book_status.idbook_status' primary_key = True))
-)
-
 class PruebaBooking(db.Model):
     __tablename__ = "book_hotel"
+    __table_args__ = {"extend_existing": True}
 
     idbook_hotel = db.Column(db.Integer, primary_key = True)
     code_reservation = db.Column(db.String(60), nullable = False)
     iddef_property = db.Column(db.Integer, db.ForeignKey("def_property.iddef_property"), nullable = False)
     short_name = db.Column(db.String(45), nullable = False)
-    trade_name = db.Colum(db.String(45), nullable = False)
+    trade_name = db.Column(db.String(45), nullable = False)
     property_code = db.Column(db.String(6), nullable = False)
     from_date = db.Column(db.DateTime)
     to_date = db.Column(db.DateTime)
     nights = db.Column(db.Integer)
     adults = db.Column(db.Integer)
+    iddef_market_segment = db.Column(db.Integer, db.ForeignKey("def_market_segment.iddef_market_segment"), nullable = False)
     currency_code = db.Column(db.String(15), nullable = False)
     description = db.Column(db.String(100), nullable = False)
     iddef_country = db.Column(db.Integer, db.ForeignKey("def_country.iddef_country"), nullable = False)
-    name = db.Colum(db.String(45), nullable = False)
+    name = db.Column(db.String(45), nullable = False)
     country_code = db.Column(db.String(45), nullable = False)
     iddef_channel = db.Column(db.Integer, db.ForeignKey("def_channel.iddef_channel"), nullable = False)
     name = db.Column(db.String(150), nullable = False)
     iddef_currency = db.Column(db.Integer, db.ForeignKey("def_currency.iddef_currency"), nullable = False)
     currency_code = db.Column(db.String(15), nullable = False)
-    description = db.Column(db.Strin(60), nullable = False)
+    description = db.Column(db.String(60), nullable = False)
     iddef_language = db.Column(db.Integer, db.ForeignKey("def_language.iddef_language"), nullable = False)
-    lang_code = db.Colum(db.String(10), nullable = False)
+    lang_code = db.Column(db.String(10), nullable = False)
     description = db.Column(db.String(60), nullable = False)
     exchange_rate = db.Column(db.Float, nullable = False)
     promo_amount = db.Column(db.Float, nullable = False)
     discount_percent = db.Column(db.Float, nullable = False)
-    discount_amount = db.Column(db.FLoat, nullable = False)
+    discount_amount = db.Column(db.Float, nullable = False)
     total_gross = db.Column(db.Float, nullable = False)
     fee_amount = db.Column(db.Float, nullable = False)
     country_fee = db.Column(db.Float, nullable = False)
@@ -58,14 +50,14 @@ class PruebaBooking(db.Model):
     code = db.Column(db.String(50), nullable = False)
     description = db.Column(db.String(100), nullable = False)
     device_request = db.Column(db.String(50), nullable = False)
-    expiry_date = db.Column(db.DateTime, nullable = False)
-    cancelation_date = db.Column(db.DateTime, nullable = False)
-    modification_date_booking = db.Column(db.DateTime, nullable = False)
+    expiry_date = db.Column(db.DateTime, default="1900-01-01 00:00:00")
+    cancelation_date = db.Column(db.DateTime, default="1900-01-01 00:00:00")
+    modification_date_booking = db.Column(db.DateTime, default="1900-01-01 00:00:00")
     estado = db.Column(db.Integer)
     usuario_creacion = db.Column(db.String(45), nullable = False)
     fecha_creacion = db.Column(db.DateTime)
     usuario_ultima_modificacion = db.Column(db.String)
-    fecha_ultima_modificacion = db.Column(db.DateTime)
+    fecha_ultima_modificacion = db.Column(db.DateTime, default="1900-01-01 00:00:00")
 
 class PruebaBookingSchema(ma.Schema):
     idbook_hotel = fields.Integer()
@@ -78,6 +70,7 @@ class PruebaBookingSchema(ma.Schema):
     to_date = fields.DateTime("%Y-%m-%d %H:%M:%S")
     nights = fields.Integer()
     adults = fields.Integer()
+    iddef_market_segment = fields.Integer()
     currency_code = fields.String(required = True, validate = validate.Length(max = 15))
     description = fields.String(required = True, validate = validate.Length(max = 100))
     iddef_country = fields.Integer()
@@ -87,7 +80,7 @@ class PruebaBookingSchema(ma.Schema):
     name = fields.String(required = True, validate = validate.Length(max = 150))
     iddef_currency = fields.Integer()
     currency_code = fields.String(required = True, validate = validate.Length(max = 15))
-    descripton = fields.Strimg(required = True, validate = validate.Length(max = 60 ))
+    descripton = fields.String(required = True, validate = validate.Length(max = 60 ))
     iddef_language = fields.Integer()
     lang_code = fields.String(required = True, validate = validate.Length(max = 10))
     description = fields.String(required = True, validate = validate.Length(max = 60))
@@ -112,7 +105,7 @@ class PruebaBookingSchema(ma.Schema):
     cancelation_date = fields.DateTime("%Y-%m-%d %H:%M:%S")
     modification_date_booking = fields.DateTime("%Y-%m-%d %H:%M:%S")
     estado = fields.Integer()
-    usuario_creacion = fields.Strig(required = True, validate = validate.Length(max = 45))
+    usuario_creacion = fields.String(required = True, validate = validate.Length(max = 45))
     fecha_creacion = fields.DateTime("%Y-%m-%d %H:%M:%S")
-    usuario_ultima_modificacion = fields.DAteTime("%Y-%m-%d %H:%M:%S")
+    usuario_ultima_modificacion = fields.DateTime("%Y-%m-%d %H:%M:%S")
     fecha_ultima_modificacion = fields.DateTime("%Y-%m-%d %H:%M:%S")
--- resources/prueba/init.py
+++
@@ -1,4 +0,0 @@
-from config import api 
-from.prueba import PruebaSearch
-
-api.add_resource(PruebaSearch, '/api/prueba/search/<int:estado>',endpoint="api-prueba-get-by-estado", methods=["GET"])
