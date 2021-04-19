8df4dc4fed4c070afecfe2905578428a6882408e
danirojasglz <danirojasglz@gmail.com>
2021-04-19 10:58:18 -0500

    Cambios en el query de la api y en el schema

--- resources/prueba/book_customer_hotel.py
+++ resources/prueba/book_customer_hotel.py
@@ -13,34 +13,43 @@ import json
 class BookHotelCustomerSearch(Resource):
     def get(self):
         try:
-            query = """select
-	            ba.iddef_country,\
-	            pr.property_code,\
-	            CONCAT_WS(' ',bc.title, bc.first_name, bc.last_name) AS guest_name,\
-	            bc.email AS guest_email,\
-	            CONCAT_WS('-',bc.dialling_code,bc.phone_number) AS guest_phone,\
-                pr.trade_name as tr_name,\
-                pr.property_code as pr_code,\
-                bs.idbook_status as bs_idBookStatus,\
-                bs.name as bs_name,\
-                bs.code as bs_code,\
-                bhr.idop_rate_plan as id_rate_plan,
-	            rtc.room_description as room_type,
-                rtc.room_code,
-
-	            rtc.room_code, bh.*\
-            
-	            FROM book_hotel bh
-            
-	            JOIN book_hotel_room bhr ON bh.idbook_hotel = bhr.idbook_hotel AND bhr.estado = 1
-	            JOIN def_property pr ON pr.iddef_property = bh.iddef_property
-                JOIN book_customer_hotel bch ON bch.idbook_hotel = bh.idbook_hotel AND bch.estado = 1
-	            JOIN book_customer bc ON bc.idbook_customer = bch.idbook_customer
-	            JOIN def_room_type_category rtc ON rtc.iddef_room_type_category = bhr.iddef_room_type
-	            JOIN book_status bs ON bs.idbook_status = bh.idbook_status
-	            JOIN book_address ba on ba.idbook_customer = bc.idbook_customer
-            
-                where bh.estado =1 order by idbook_hotel desc limit 20;  """
+            query = """SELECT 
+                        ba.iddef_country,
+                        pr.property_code,
+                        CONCAT_WS(' ',
+                                bc.title,
+                                bc.first_name,
+                                bc.last_name) AS guest_name,
+                        bc.email AS guest_email,
+                        CONCAT_WS('-', bc.dialling_code, bc.phone_number) AS guest_phone,
+                        bhr.iddef_room_type AS room_type_id,
+                        CONCAT_WS('-', rtc.room_description, rtc.room_code) AS room_type,
+                        pr.trade_name AS hotel_name,
+                        bhr.idop_rate_plan AS id_rate_plan,
+                        rtp.code AS rate_plan,
+                        bh.*
+                    FROM
+                        book_hotel bh
+                            JOIN
+                        book_hotel_room bhr ON bh.idbook_hotel = bhr.idbook_hotel
+                            AND bhr.estado = 1
+                            JOIN
+                        def_property pr ON pr.iddef_property = bh.iddef_property
+                            JOIN
+                        book_customer_hotel bch ON bch.idbook_hotel = bh.idbook_hotel
+                            AND bch.estado = 1
+                            JOIN
+                        book_customer bc ON bc.idbook_customer = bch.idbook_customer
+                            JOIN
+                        def_room_type_category rtc ON rtc.iddef_room_type_category = bhr.iddef_room_type
+                            JOIN
+                        book_status bs ON bs.idbook_status = bh.idbook_status
+                            JOIN
+                        book_address ba ON ba.idbook_customer = bc.idbook_customer
+                    		JOIN
+                    	op_rateplan rtp ON rtp.idop_rateplan = bhr.idop_rate_plan
+                    WHERE
+                        bh.estado = 1 """
 
             book_hotel = db.session.execute(query).fetchall()
             for row in book_hotel:
